94c9913e8f24d1c13fb18e2e01baf8ffb4573d0b
Straughter "BatmanOsama" Guthrie <56135400+jmanhype@users.noreply.github.com>
2024-11-09 14:05:41 -0600

    Update entrypoint.sh fixed run command

--- scripts/gradio/entrypoint.sh
+++ scripts/gradio/entrypoint.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-echo "entrypoint.sh"
+set -e  # Exit immediately if a command exits with a non-zero status
+
+echo "Starting entrypoint.sh"
 whoami
 which python
-export PYTHONPATH=${PYTHONPATH}:/home/user/app/MuseV:/home/user/app/MuseV/MMCM:/home/user/app/MuseV/diffusers/src:/home/user/app/MuseV/controlnet_aux/src
-echo "pythonpath" $PYTHONPATH
-# chmod 777 -R /home/user/app/MuseV
-# Print the contents of the diffusers/src directory
-# echo "Contents of /home/user/app/MuseV/diffusers/src:"
-# Load ~/.bashrc
-# source ~/.bashrc
 
+# Export PYTHONPATH
+export PYTHONPATH=${PYTHONPATH}:/home/user/app/MuseV:/home/user/app/MuseV/MMCM:/home/user/app/MuseV/diffusers/src:/home/user/app/MuseV/controlnet_aux/src
+echo "PYTHONPATH: $PYTHONPATH"
+
+# Activate the conda environment
 source /opt/conda/etc/profile.d/conda.sh
 conda activate musev
+
 which python
-python ap_space.py
+
+# Navigate to Gradio scripts directory
+cd /home/user/app/MuseV/scripts/gradio/
+
+# Run the Gradio application
+exec python app_gradio_space.py
